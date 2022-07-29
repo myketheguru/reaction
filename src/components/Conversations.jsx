0ac788b3455f907a948fdff3e7129c5ae1807876
myketheguru <darkmyketheguru@gmail.com>
2022-07-29 16:06:13 +0100

    Refactored Conversations UI

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -1,112 +1,179 @@
-import React from "react";
-import { AiOutlineAppstore } from "react-icons/ai";
-import { TbNotes, TbSend, TbPaperclip } from "react-icons/tb";
-import { BiMicrophone } from "react-icons/bi";
-import { HiOutlineVideoCamera } from "react-icons/hi";
+import { useEffect, useRef } from "react";
+import { TbNotes } from "react-icons/tb";
+import { AiOutlinePaperClip } from "react-icons/ai";
+import { HiOutlineVideoCamera, HiPaperAirplane } from "react-icons/hi";
 import { FiPhoneCall } from "react-icons/fi";
 import { CgImage } from "react-icons/cg";
 import Img from "../Conversations-Images/IMG_0394.jpg";
+import me from '../assets/12345.jpg'
+import { RiApps2Line } from "react-icons/ri";
+import { BsDot, BsMic } from "react-icons/bs";
+import { FaPen } from "react-icons/fa";
 
 const Conversations = () => {
+  const convo = useRef()
+
+  useEffect(() => {
+    if (!convo.current) return
+    const timeId = setInterval(() => {
+      convo.current.scrollTo(0, 500)
+    }, 1000);
+  
+    return () => {
+      clearInterval(timeId)
+    }
+  }, [])
+
   return (
-    <div className="conversations text-white grid grid-rows-[auto_1fr_auto] overflow-auto no-bar">
+    <div className="conversations text-white grid grid-rows-[auto_1fr_auto] overflow-auto scroll-smooth no-bar" ref={convo}>
       {/* Conversations */}
-      <nav className="flex justify-between h-20 items-center p-8 sticky top-0 bg-[#161719] z-10">
-        <div className="flex gap-4">
-          <div>
-            <AiOutlineAppstore size={24} />
-          </div>
-          <div className="flex gap-2">
+      <nav className="flex justify-between h-[70px] items-center p-8 sticky top-0 bg-[linear-gradient(to_bottom,_#161719,70%,_#161719ee)] shadow-xl shadow-[#161719ee] z-10">
+        <div className="flex gap-4 items-center">
+          <button className="text-gray-400">
+            <RiApps2Line size={22} />
+          </button>
+          <div className="flex gap-2 text-[18px] items-center font-medium">
             <div>Chat</div>
-            <div>{24}</div>
+            <div className="text-[15px] font-normal text-gray-400">{24}</div>
           </div>
         </div>
-        <div className="flex gap-6">
-          <figure>
-            {" "}
+        <div className="flex gap-6 text-gray-500">
+          <button>
             <HiOutlineVideoCamera size={24} />
-          </figure>
-          <figure>
-            <FiPhoneCall size={24} />
-          </figure>
-          <figure>
-            <CgImage size={24} />
-          </figure>
-          <figure>
-            <TbNotes size={24} />
-          </figure>
+          </button>
+          <button>
+            <FiPhoneCall size={20} />
+          </button>
+          <button>
+            <CgImage size={20} />
+          </button>
+          <button>
+            <TbNotes size={21} />
+          </button>
         </div>
 
         <div className="flex gap-2">
           <figure className="h-[24px] w-[24px] bg-white rounded-full overflow-hidden">
-          <img src={Img} alt="" />
+          <img src={me} alt="" />
           </figure>
           <figure className="h-[24px] w-[24px] bg-white rounded-full overflow-hidden">
           <img src={Img} alt="" />
           </figure>
-          <figure className="h-[24px] w-[24px] bg-white rounded-full overflow-hidden">
-          6+
+          <figure className="h-[24px] w-[24px] bg-[#2b2e30] text-[10px] flex justify-center items-center rounded-full overflow-hidden">
+            6+
           </figure>
         </div>
       </nav>
 
-      <div className="px-16 flex flex-col gap-4">
-        <p className="flex justify-center">Wed 27 Jul 1:25pm</p>
+      <div className="px-16 py-3 flex flex-col gap-4 text-xs">
+        <p className="flex justify-center text-gray-400 text-[11px]">Fri 29 Jul 2:42pm</p>
         <div className="flex flex-col items-end relative gap-2">
           <div>
-            <span> 2:00pm</span>
             <span> You</span>
-            <figure className="h-[32px] w-[32px] bg-white rounded-full absolute -right-10 -top-[3px]"></figure>
+            <span className="text-gray-500"> 2:00pm</span>
+            <figure className="h-[32px] w-[32px] bg-[#161719] rounded-full absolute -right-10 -top-[3px]">
+              <img src={me} alt="" className="rounded-full" />
+            </figure>
           </div>
-          <p className="bg-[#43A0FF] py-2 px-4 rounded-full">
-            Lorem ipsum dolor sit amet consectetur adipi
+          <p className="bg-[#43A0FF] py-2 px-4 rounded-full rounded-tr-none">
+            My baby ❤️
           </p>
           <p className="bg-[#43A0FF] py-2 px-4 rounded-full">
-            {" "}
-            amet consectetur adipi
+            Send me some jpegs naw 😢
           </p>
-          {/* images come here */}
-          <figure className="h-[250px] w-[200px] bg-white rounded-lg overflow-hidden">
-          <img src={Img} alt="" />
-          </figure>
         </div>
 
         <div className="flex flex-col items-start relative gap-2">
           <div>
-            <span> 2:00pm</span>
-            <span> You</span>
-            <figure className="h-[32px] w-[32px] bg-white rounded-full absolute -left-10 -top-[3px]"></figure>
+            <span>Fine Babe</span>
+            <span className="text-gray-500"> 2:02pm</span>
+            <figure className="h-[32px] w-[32px] bg-white rounded-full absolute -left-10 -top-[3px] overflow-hidden">
+              <img src={Img} alt="" />
+            </figure>
           </div>
-          <p className="bg-[#2c3436] py-2 px-4 rounded-full">
-            Lorem ipsum dolor sit amet consectetur adipi
+          <p className="bg-[#2c3436] py-2 px-4 rounded-full rounded-tl-none">
+            Okay sweetie
           </p>
-          <p className="bg-[#2c3436] py-2 px-4 rounded-full">
-            {" "}
-            amet consectetur adipi
+          <div className="img-group flex gap-2">
+            <figure className="h-[250px] w-[200px] bg-white rounded-lg overflow-hidden">
+              <img src={Img} alt="" />
+            </figure>
+            <figure className="h-[250px] w-[200px] bg-white rounded-lg overflow-hidden">
+              <img src={Img} alt="" />
+            </figure>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-end relative gap-2">
+          <div>
+            <span> You</span>
+            <span className="text-gray-500"> 2:05pm</span>
+            <figure className="h-[32px] w-[32px] bg-[#161719] rounded-full absolute -right-10 -top-[3px]">
+              <img src={me} alt="" className="rounded-full" />
+            </figure>
+          </div>
+          <p className="bg-[#43A0FF] py-2 px-4 rounded-full rounded-tr-none">
+            Soo Pretty! I'm in love 😍
           </p>
-          <figure className="h-[250px] w-[200px] bg-white rounded-lg overflow-hidden">
-          <img src={Img} alt="" />
-          </figure>
+          <p className="bg-[#43A0FF] py-2 px-4 rounded-full">
+            Why did you send it twice, though? 😂
+          </p>
+        </div>
+
+        <div className="flex flex-col items-start relative gap-2">
+          <div>
+            <span>Fine Babe</span>
+            <span className="text-gray-500"> 2:06pm</span>
+            <figure className="h-[32px] w-[32px] bg-white rounded-full absolute -left-10 -top-[3px] overflow-hidden">
+              <img src={Img} alt="" />
+            </figure>
+          </div>
+          <p className="bg-[#2c3436] py-2 px-4 rounded-full rounded-tl-none">
+            So that you see only me. Just want you to be mine, baby.
+          </p>
+        </div>
+        
+        <div className="flex flex-row items-start relative">
+          <div>
+            <FaPen className="absolute -left-10 top-[1px] text-yellow-400" />
+            <figure className="h-[20px] w-[20px] bg-white rounded-full absolute -left-4 -top-[3px] overflow-hidden">
+              <img src={Img} alt="" />
+            </figure>
+          </div>
+          <p className="rounded-full rounded-tl-none px-4 text-gray-400">
+            <span className="text-yellow-400">Fine Babe </span> is typing...
+          </p>
         </div>
       </div>
 
-      <footer className="flex gap-4 bg-[#2c3436] rounded-lg mx-8 my-2 h-12 items-center p-4 sticky bottom-2">
-        <figure className="h-[32px] w-[32px] bg-white rounded-full"></figure>
-        <input
-          type="text"
-          placeholder="Your Message"
-          className="bg-[#2c3436] outline-none"
-        />
+      <footer className="flex gap-4 bg-[#161719] h-16 items-center px-5 sticky bottom-0">
+        <div className="send-msg flex w-full bg-[#23292b] h-10 rounded-md p-3 py-2 gap-2">
+          <figure className="h-[25px] w-[25px] bg-white rounded-full flex flex-shrink-0 overflow-hidden
+          ">
+            <img src={me} alt="" />
+          </figure>
+          <input
+            type="text"
+            placeholder="Your Message"
+            className="outline-none bg-transparent text-xs text-gray-400 flex w-full"
+          />
 
-        <div className="flex gap-2 items-center ml-40">
-          <TbNotes />
-          <p>1 unsaved draft</p>
-        </div>
-        <span>.</span>
-        <div className="flex gap-2">
-          <BiMicrophone size={24} />
-          <TbPaperclip size={24} />
-          <TbSend size={24} />
+          <div className="flex gap-2 items-center whitespace-nowrap bg-[#333b3d] px-2 cursor-pointer rounded-full">
+            <TbNotes className="text-yellow-500" />
+            <p className="text-[11px] text-gray-300">1 unsaved draft</p>
+          </div>
+          <BsDot className="self-center text-gray-500" size={28} />
+          <div className="flex items-center gap-2">
+            <button className="flex items-center justify-center p-[1px] text-gray-400">
+              <BsMic size={17} />
+            </button>
+            <button className="flex items-center justify-center p-[1px] text-gray-400">
+              <AiOutlinePaperClip size={20} />
+            </button>
+            <button className="flex items-center justify-center p-[1px]">
+              <HiPaperAirplane className="rotate-45" size={19} />
+            </button>
+          </div>
         </div>
       </footer>
     </div>
